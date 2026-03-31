68f160dca0757782b8801e10cc32a98822e9b1bd
Renato Bellotti <184365954+RenatoBellottiCH@users.noreply.github.com>
2026-03-31 16:14:10 +0200

    Design: Improve tooltips

--- src/components/RadarMarker.tsx
+++ src/components/RadarMarker.tsx
@@ -12,13 +12,7 @@ const radarIcon = L.divIcon({
   iconAnchor: [12, 12], // center the icon
 });
 
-export default function MonostaticRadarMarker({
-  radar,
-  onClick,
-}: {
-  radar: Radar;
-  onClick: () => void;
-}) {
+function MonostaticRadarTooltip({ radar }: { radar: Radar }) {
   // Format the tooltip.
   const latStr = radar.receiver.point.lat.toFixed(4);
   const lonStr = radar.receiver.point.lon.toFixed(4);
@@ -34,6 +28,70 @@ export default function MonostaticRadarMarker({
     "\u00A0".repeat(maxBefore - s.indexOf(".")) + s;
 
   return (
+    <Tooltip direction="top" offset={[0, -10]}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "auto auto",
+          columnGap: "8px",
+          fontFamily: "monospace",
+        }}
+      >
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Radar ID:
+        </span>
+        <span>{radar.receiver.id}</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>Lat:</span>
+        <span>{alignDecimal(latStr)}°</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>Lon:</span>
+        <span>{alignDecimal(lonStr)}°</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Alt [MASL]:
+        </span>
+        <span>{alignDecimal(altStr)}</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Frequency:
+        </span>
+        <span>
+          {alignDecimal((radar.transmitter.frequency / 1000).toFixed(2))} GHz
+        </span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Bandwidth:
+        </span>
+        <span>
+          {alignDecimal(radar.receiver.bandwidth.toFixed(2))} MHz
+        </span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Noise temperature:
+        </span>
+        <span>
+          {alignDecimal(radar.receiver.noise_temperature.toFixed(2))} K
+        </span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Prob. false alarm:
+        </span>
+        <span>
+          {alignDecimal(radar.receiver.pfa.toExponential(2))}
+        </span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Rotation time:
+        </span>
+        <span>
+          {alignDecimal(radar.receiver.rotation_time.toFixed(2))} s
+        </span>
+      </div>
+    </Tooltip>
+  );
+}
+
+export default function MonostaticRadarMarker({
+  radar,
+  onClick,
+}: {
+  radar: Radar;
+  onClick: () => void;
+}) {
+  return (
     <Marker
       position={[radar.receiver.point.lat, radar.receiver.point.lon]}
       icon={radarIcon}
@@ -41,29 +99,7 @@ export default function MonostaticRadarMarker({
         click: onClick,
       }}
     >
-      <Tooltip direction="top" offset={[0, -10]}>
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto auto",
-            columnGap: "8px",
-            fontFamily: "monospace",
-          }}
-        >
-          <span style={{ textAlign: "right", fontWeight: "bold" }}>
-            Radar ID:
-          </span>
-          <span>{radar.receiver.id}</span>
-          <span style={{ textAlign: "right", fontWeight: "bold" }}>Lat:</span>
-          <span>{alignDecimal(latStr)}°</span>
-          <span style={{ textAlign: "right", fontWeight: "bold" }}>Lon:</span>
-          <span>{alignDecimal(lonStr)}°</span>
-          <span style={{ textAlign: "right", fontWeight: "bold" }}>
-            Alt [MASL]:
-          </span>
-          <span>{alignDecimal(altStr)}</span>
-        </div>
-      </Tooltip>
+      <MonostaticRadarTooltip radar={radar} />
     </Marker>
   );
 }
--- src/components/TrajectoryLayer.tsx
+++ src/components/TrajectoryLayer.tsx
@@ -32,10 +32,46 @@ export default function TrajectoryLayer({
   );
 
   const id = "target_id" in trajectory ? trajectory.target_id : trajectory.id;
+
+  const latStr = currentPoint.lat.toFixed(4);
+  const lonStr = currentPoint.lon.toFixed(4);
+  const altStr = currentPoint.alt.toFixed(0);
+
+  const maxBefore = Math.max(
+    latStr.indexOf("."),
+    lonStr.indexOf("."),
+    altStr.indexOf("."),
+  );
+
+  const alignDecimal = (s: string) =>
+    "\u00A0".repeat(maxBefore - s.indexOf(".")) + s;
+
   const tooltip = (
-    <Tooltip>
-      <div>
-        {"target_id" in trajectory ? `Target ID: ${id}` : `Track ID: ${id}`}
+    <Tooltip direction="top" offset={[0, -10]}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "auto auto",
+          columnGap: "8px",
+          fontFamily: "monospace",
+        }}
+      >
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          {"target_id" in trajectory ? "Target ID:" : "Track ID:"}
+        </span>
+        <span>{id}</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Latitude:
+        </span>
+        <span>{alignDecimal(latStr)} °</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Longitude:
+        </span>
+        <span>{alignDecimal(lonStr)} °</span>
+        <span style={{ textAlign: "right", fontWeight: "bold" }}>
+          Altitude [MASL]:
+        </span>
+        <span>{altStr} m</span>
       </div>
     </Tooltip>
   );
@@ -53,7 +89,9 @@ export default function TrajectoryLayer({
         color="red"
         fillColor="red"
         fillOpacity={100}
-      />
+      >
+        {tooltip}
+      </CircleMarker>
       <Polyline
         color={isBlue ? "blue" : "red"}
         positions={futurePoints.map((p) => [p.lat, p.lon])}
